763d5517a8a241583e8b04a208dc913931ccb143
Dan Tra <dltra@fcps.edu>
2021-04-27 10:59:47 -0400

    Create DrawView class extending SurfaceView and implementing Runnable Instantiate DrawView in activity_main.xml Implement run() in DrawView 	Access and lock Canvas 	Draw 	Release Canvas 	Calculate time remaining to match Frames Per Second 	Pause until time requirement matched Handle onPause and onResume events 	onPause -> terminate thread and rejoin main thread 	onResume -> instantiate and start thread

--- app/src/main/java/com/example/surfaceviewexample/MainActivity.java
+++ app/src/main/java/com/example/surfaceviewexample/MainActivity.java
@@ -5,10 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawView mDrawView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawView = findViewById(R.id.drawView);
+    }
+    /**
+     * Pauses game when activity is paused.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDrawView.pause();
+    }
+
+    /**
+     * Resumes game when activity is resumed.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDrawView.resume();
     }
 }
